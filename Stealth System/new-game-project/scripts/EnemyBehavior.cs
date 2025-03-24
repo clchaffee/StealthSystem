7f329444bd1eeec18c6aa3ea41648ade408a911e
clchaffee <conradlchaffee@gmail.com>
2025-03-23 23:36:07 -0400

    final push for submission

--- Stealth System/new-game-project/scripts/EnemyBehavior.cs
+++ Stealth System/new-game-project/scripts/EnemyBehavior.cs
@@ -3,5 +3,9 @@ using System;
 
 public partial class Enemy : Node3D
 {
-	
+	//@export CharacterBody3D player;
+	//@export int speed = 50;
+	//
+	//@onready RayCast3D rayCast = $CSGMesh3D/RayCast3D;
+	//@onready Timer timer = $Timer; 
 }
